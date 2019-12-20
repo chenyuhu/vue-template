b8d26c0799b172a6d156ce8102d19a9c00dc92f4
陈锋 <1030304751@qq.com>
2019-12-20 15:26:08 +0800

    初始化提交

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: '@sishuguojixuefu',
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'import/extensions': 0,
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+}
